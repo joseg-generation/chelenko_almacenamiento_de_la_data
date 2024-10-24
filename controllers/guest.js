3ef2e2b6b6a1899fbd3009f152a6d2e276bb23e7
DayanaAlv <yoendryalvarezg@gmail.com>
2024-10-24 12:16:03 -0300

    cambios 2.0

--- controllers/guest.js
+++ controllers/guest.js
@@ -1,102 +1,45 @@
-const guestsRouter = require ('express').Router();
-const {Guest} = require ('../models/index');
+const guestsRouter = require("express").Router();
+const { Guest } = require("../models/index");
 
-// lista completa 
-guestsRouter.get('/', (req, res, next) => {
-    Guest.find({})
+guestsRouter.get("/", (req, res) => {
+  Guest.find({})
     .then((guests) => res.json(guests))
     .catch((error) => res.status(500).json({ error: "Error al obtener huéspedes" }));
 });
 
-// lista por id 
-guestsRouter.get('/:id' ,(req, res, next) => {
-    Guest.findById(req.params.id)
-     .then (existingGuest => {
-        if (existingGuest) {
-            res.json(existingGuest);
-        } else {
-            res.status(404).end()
-        }
-     })
-     .catch(error => next(error));
-});
-
-// agregar a la lista 
- guestsRouter.post('/' , (req,res, next) => {
-    const body = req.body
-    if (!body.firstName || !body.lastName || !body.email) {
-        return res.status(400).json({ error: "Datos faltantes: firstName, lastName o email" });
+guestsRouter.get("/:id", (req, res, next) => {
+  Guest.findById(req.params.id)
+    .then((guest) => {
+      if (guest) {
+        res.json(guest);
+      } else {
+        res.status(404).end();
       }
-
-    const guest = new Guest({
-        firstName: body.firstName,
-        lastName: body.lastName,
-        email: body.email,
-        phone: body.phone ||  undefined,
-        nationality: body.nationality || undefined,
-        documentType: body.documentType || undefined,
-        documentNumber: body.documentNumber || undefined,
-        address: body.address || {},
-        preferences: body.preferences ? new Map(body.preferences) : new Map(),
-        blacklisted: body.blacklisted || false,
-        blacklistReason: body.blacklistReason || undefined
-    });
-    guest.save()
-    .then(savedGuest => { 
-    res.status(201).json(savedGuest);
-     })
-     .catch(error => next(error));
+    })
+    .catch((error) => next(error));
 });
 
-// Editas la lista
-guestsRouter.put('/:id', (req, res, next) => {
-    const body = req.body;
+guestsRouter.post("/", (req, res, next) => {
+  const body = req.body;
 
-    Guest.findById(req.params.id)
-        .then((existingGuest) => {
-            if (!existingGuest) {
-                return res.status(404).end();
-            }
-            
-            const updatedAddress = {
-                street: body.address?.street !== undefined ? body.address.street : existingGuest.address.street,
-                city: body.address?.city !== undefined ? body.address.city : existingGuest.address.city,
-                state: body.address?.state !== undefined ? body.address.state : existingGuest.address.state,
-                country: body.address?.country !== undefined ? body.address.country : existingGuest.address.country,
-                postalCode: body.address?.postalCode !== undefined ? body.address.postalCode : existingGuest.address.postalCode
-            };
+  if (!body.firstName || !body.lastName || !body.email) {
+    return res.status(400).json({ error: "Datos faltantes: firstName, lastName o email" });
+  }
 
-            const guest = {
-                firstName: body.firstName !== undefined ? body.firstName : existingGuest.firstName,
-                lastName: body.lastName !== undefined ? body.lastName : existingGuest.lastName,
-                email: body.email !== undefined ? body.email : existingGuest.email,
-                phone: body.phone !== undefined ? body.phone : existingGuest.phone,
-                nationality: body.nationality !== undefined ? body.nationality : existingGuest.nationality,
-                documentType: body.documentType !== undefined ? body.documentType : existingGuest.documentType,
-                documentNumber: body.documentNumber !== undefined ? body.documentNumber : existingGuest.documentNumber,
-                address: updatedAddress,
-                preferences: body.preferences !== undefined ? new Map(body.preferences) : existingGuest.preferences,
-                blacklisted: body.blacklisted !== undefined ? body.blacklisted : existingGuest.blacklisted,
-                blacklistReason: body.blacklistReason !== undefined ? body.blacklistReason : existingGuest.blacklistReason
-            };
+  const guest = new Guest({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    phone: body.phone || undefined,
+    nationality: body.nationality || undefined,
+    documentType: body.documentType || undefined,
+    documentNumber: body.documentNumber || undefined,
+    address: body.address || {},
+    preferences: body.preferences ? new Map(body.preferences) : new Map(),
+    blacklisted: body.blacklisted || false,
+    blacklistReason: body.blacklistReason || undefined
+  });
 
-            return Guest.findByIdAndUpdate(req.params.id, guest, { new: true })
-                .then((updatedGuest) => {
-                    res.json(updatedGuest);
-                })
-                .catch(error => next(error));
-        })
-        .catch(error => next(error));
-});
-
-//borrar
- guestsRouter.delete('/:id', (req, res, next) => {
-    Guest.findByIdDelete(req.params.id)
-      .then(() => {
-        res.status(204).end();
-      })
-      .catch(error => next(error));
-  })
   guest
     .save()
     .then((savedGuest) => res.status(201).json(savedGuest))
