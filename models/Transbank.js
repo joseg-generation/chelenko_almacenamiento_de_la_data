98b5f7c38195342a0b84891b8c10f91feab18b26
Lechuzascript <camilo.gonzalez@genstudents.org>
2024-10-30 12:09:47 -0300

    Creación transbank Schema

---
+++ models/Transbank.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+const transbankSchema = new mongoose.Schema({
+guest: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Guest',
+    required: true
+},
+vci: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['INITIALIZED', 'AUTHORIZED', 'REVERSED', 'FAILED', 'NULLIFIED', 'PARTIALLY_NULLIFIED', 'CAPTURED'],
+    default: 'INITIALIZED'
+  },
+  buyOrder: {
+    type: String,
+    required: true
+  },
+  sessionId: {
+    type: String,
+    required: true
+  },
+  cardDetail: {
+    cardNumber: {
+        type: Number,
+        maxLength: 19
+    }
+  },
+  accountingDate: {
+    type: String,
+    maxLength: 4,
+    required: true
+  },
+  transactionDate: {
+    type: Date,
+    required: true
+  },
+  autorizationCode: {
+    type: String,
+    maxLength: 6,
+    required: true
+  },
+  paymentTypeCode: {
+    type: String,
+    required: true,
+    enum: ['VD', 'VN', 'VC', 'S1', 'S2', 'NC', 'VP']
+  },
+  responseCode: {
+    type: Number,
+    required: true
+  },
+  installmentsAmount: {
+    type: Number,
+    maxLength: 17
+  },
+  installmentsNumber: {
+    type: Number,
+    maxLength: 2,
+    default: 0
+  },
+  balance: {
+    type: Number,
+    maxLength: 17
+  }  
+}, { timestamps: true });
+
+module.exports = mongoose.model('Transbank', transbankSchema);
